1e8153e6ec29d678c57cfc5f773dc3c57e97d784
chenwei1905 <chenwei1905@126.com>
2017-09-08 23:44:32 +0800

    Delete m3u8.py

--- m3u8/m3u8.py
+++
@@ -1,6 +0,0 @@
-http://58.220.102.229:1970/otv/nyz/live/channel12/700.m3u8 WEIQI
-http://guoguang.live.otvcloud.com/otv/xjgg/live/channel17/700.m3u8 MEISHI
-http://58.220.102.229:1970/otv/nyz/live/channel31/700.m3u8 JINBAO
-http://222.68.17.119/otv/wxty/live/channel01/700.m3u8 WUXING
-
-
